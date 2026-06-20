3dcbf4bc6660883968e51aff75424aeba7c2ccd3
SC1314520 <sclemon1013@gmail.com>
2026-06-20 21:41:41 +0800

    VSlider 優化

--- src/components/VSlider/VSlider.vue
+++ src/components/VSlider/VSlider.vue
@@ -1,18 +1,21 @@
 <template>
     <div class="v-slider-controller-progress-wrapper-all">
-        <div class="v-slider-prepend-slot" v-if="hasSlot('prepend')"><slot name="prepend"></slot></div>
-        <div class="v-slider-controller-progress-wrapper" ref="progressWrapperRef" 
+        <div class="v-slider-prepend-slot" v-if="hasSlot('prepend')" :style="{marginRight: circleRadius}"><slot name="prepend"></slot></div>
+        <div class="v-slider-controller-progress-wrapper" ref="progressWrapperRef" :style="{height : `calc(${height} + 10px)`}"
             :class="{ 'v-slider-controller-progress-wrapper-is-disabled': disabled }"
             @pointerdown.stop="startDrag" @pointermove.stop="dragTo" @pointerup.stop="stopDrag" @pointercancel.stop="stopDrag">
-            <div class="v-slider-controller-progress-track"></div>
-            <div class="v-slider-controller-progress" :class="{ 'v-slider-controller-progress-is-disabled': disabled }" :style="{ width: progress + '%', background: (disabled ? disabledColor: color)}"></div>
-            <div class="v-slider-controller-progress-controller" :style="{ left: progress + '%', background: (disabled ? disabledColor: color) }" @mouseenter="isHover = true" @mouseleave="isHover = false">
+            <div class="v-slider-controller-progress-track" :style="{ height }"></div>
+            
+            <div class="v-slider-controller-progress" :class="{ 'v-slider-controller-progress-is-disabled': disabled }" :style="{ width: progress + '%', height, background: (disabled ? disabledColor: color)}"></div>
+            
+            <div class="v-slider-controller-progress-controller" :style="{ left: progress + '%', background: (disabled ? disabledColor: color), height: circleRadius }"
+            @mouseenter="isHover = true" @mouseleave="isHover = false">
                 <VDropdown class="v-slider-controller-progress-controller-dropdown" placement="top" minHeight="22.5px" width="35px" v-if="isHover || isDragging">
                     <div class="v-slider-value">{{ progress.toFixed(1) }}</div>
                 </VDropdown>
             </div>
         </div>
-        <div class="v-slider-append-slot" v-if="hasSlot('append')"><slot name="append"></slot></div>
+        <div class="v-slider-append-slot" v-if="hasSlot('append')" :style="{marginLeft: circleRadius}"><slot name="append"></slot></div>
     </div>
 </template>
 
@@ -30,6 +33,14 @@ export default {
         modelValue:{
             type: Number,
             default: 0,
+        },
+        height: {
+            type: String,
+            default: '4.5px'
+        },
+        circleRadius:{
+            type: String,
+            default: '14.5px'
         },
         color:{
             type: String,
@@ -76,30 +87,23 @@ export default {
         position: relative;
         flex: 1;
         margin: 0 auto;
-        height: 13px;
     }
 
     .v-slider-controller-progress-track{
         position: absolute;
         width: 100%;
-        height: 3px;
         background: rgba(255,255,255,.12);
-
         left: 0;
         top: 50%;
         transform: translateY(-50%);
-
         transition: height .15s ease, transform .15s ease;;
     }
 
     .v-slider-controller-progress{
         position: absolute;
-        height: 3px;
-
         left: 0;
         top: 50%;
         transform: translateY(-50%);
-
         transition: height .15s ease, transform .15s ease;
     }
 
@@ -110,10 +114,9 @@ export default {
 
     .v-slider-controller-progress-controller{
         position: absolute;
-        width: 12px;
-        height: 12px;
+        aspect-ratio: 1/1;
         border-radius: 50%;
-
+        
         top: 50%;
         left: 0;
 
@@ -137,12 +140,12 @@ export default {
 
     .v-slider-controller-progress-wrapper:not(.v-slider-controller-progress-wrapper-is-disabled):hover
     .v-slider-controller-progress-track{
-        transform: translateY(-50%) scaleY(1.5);
+        transform: translateY(-50%) scaleY(1.2);
     }
 
     .v-slider-controller-progress-wrapper:not(.v-slider-controller-progress-wrapper-is-disabled):hover
     .v-slider-controller-progress{
-        transform: translateY(-50%) scaleY(1.5);
+        transform: translateY(-50%) scaleY(1.2);
     }
 
     /* dropdown */
@@ -159,11 +162,9 @@ export default {
     /* slot */
     .v-slider-prepend-slot{
         color: white;
-        margin-right: 12px;
     }
     .v-slider-append-slot{
         color: white;
-        margin-left: 12px;
     }
 
     /* disabled */
